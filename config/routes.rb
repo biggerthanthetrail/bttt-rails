701720be6631bd50b05ce1775c22f6a2209c4dc5
TommyByrne <tbyrn687@gmail.com>
2018-01-24 15:34:52 -0600

    add back donate page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :charges, only: [:new], path: 'donate'
   resources :charges, only: [:create], path: 'thanks'
 
-  get '/donate' => redirect('/donate/new')
+  # get '/donate' => redirect('/donate/new')
 end
